058fe2e1b6d6c935e7bf1c57ae3a54b33e78da7e
william.venus <william.venus@mv.com.br>
2025-02-22 14:43:59 -0300

    committeste

--- src/main/java/com/project/professor/alocation/ProfessorAlocationApplication.java
+++ src/main/java/com/project/professor/alocation/ProfessorAlocationApplication.java
@@ -9,5 +9,6 @@ public class ProfessorAlocationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProfessorAlocationApplication.class, args);
 	}
+	// Comentando aqui para testar
 
 }
