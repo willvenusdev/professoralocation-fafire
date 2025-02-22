624c8412e927cb37c8973fefd61c405effe95199
william.venus <william.venus@mv.com.br>
2025-02-22 14:09:45 -0300

    feat: add initial project

---
+++ src/test/java/com/project/professor/alocation/ProfessorAlocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professor.alocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAlocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
